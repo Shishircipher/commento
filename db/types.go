9a7c7c7f11d7674fb335f3ee139a37af01ab147c
Shishir Chaurasiya <shishirchaurasiya17@gmail.com>
2025-03-30 15:09:24 +0000

    Feat : User_public type added

--- db/types.go
+++ db/types.go
@@ -2,9 +2,28 @@ package db
 
 // Comment struct
 type Comment struct {
-        ID       int    `json:"id"`
-        PostID   int    `json:"post_id"`
-        ParentID *int   `json:"parent_id"`
+	ID       int    `json:"id"`
+	PostID   int    `json:"post_id"`
+	ParentID *int   `json:"parent_id"`
+	Content  string `json:"content"`
+	AuthorID int    `json:"author_id"`
+	Author   User_public `json:"author"` // Add author details
+}
+
+type User struct {
+        Name    string `json:"name"`
+        Email   string `json:"email"`
+        Picture string `json:"picture"`
+}
+
+type NewComment struct {
+        Author   User  `json:"author"`
+        PostID   int   `json:"post_id"`
+        ParentID *int  `json:"parent_id"`
         Content  string `json:"content"`
-        AuthorID int    `json:"author_id"`
+}
+
+type User_public struct {
+	Name    string `json:"name"`
+        Picture string `json:"picture"`
 }
